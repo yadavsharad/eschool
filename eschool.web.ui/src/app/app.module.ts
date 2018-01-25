b7d2a698964a352fe0b725f236d7cf46ea1113d1
yadavsharad <yadav_sharad@hotmail.com>
2018-01-26 02:26:06 +0530

    Added Student component

--- eschool.web.ui/src/app/app.module.ts
+++ eschool.web.ui/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student/student-list/student-list.component';
+import { StudentNewComponent } from './student/student-new/student-new.component';
+import { StudentDeleteComponent } from './student/student-delete/student-delete.component';
+import { StudentDetailsComponent } from './student/student-details/student-details.component';
+ 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    StudentNewComponent,
+    StudentDeleteComponent,
+    StudentDetailsComponent,
+ 
   ],
   imports: [
     BrowserModule
---
+++ eschool.web.ui/src/app/student/student-new/student-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-new',
+  templateUrl: './student-new.component.html',
+  styleUrls: ['./student-new.component.css']
+})
+export class StudentNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
