4faa327b53c70e258b4d6193b69feaaf13fa57a3
yadavsharad <yadav_sharad@hotmail.com>
2018-01-27 02:27:33 +0530

    Added Mat Grid - Material design
    merged changes

--- eschool.web.ui/src/app/app.module.ts
+++ eschool.web.ui/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatFormFieldControl } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
-
-
+import { MatTableModule } from '@angular/material';
+import { MatPaginatorModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { StudentListComponent } from './student/student-list/student-list.component';
 import { StudentNewComponent } from './student/student-new/student-new.component';
 import { StudentDeleteComponent } from './student/student-delete/student-delete.component';
 import { StudentDetailsComponent } from './student/student-details/student-details.component';
- 
+
 
 
 @NgModule({
@@ -17,10 +20,10 @@ import { StudentDetailsComponent } from './student/student-details/student-detai
     StudentNewComponent,
     StudentDeleteComponent,
     StudentDetailsComponent,
- 
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
